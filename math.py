c212b40779bcff708d43c0a67da761ab2f9d448e
Aashay Wadkar <aashay.0001@gmail.com>
2021-08-28 06:30:23 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#Add Implementation
+def add(x,y):
+	return x+y
+
+#Sub Implementation
+def subtract(x,y):
+	return x-y #Master
+
+#Mul Implementation
+def multiply(x,y):
+	return x*y #Bug456
+
+#Div Implementation
+def divide(x,y):
+	return x/y #Bug789
+
+def square(x):
+	pass
